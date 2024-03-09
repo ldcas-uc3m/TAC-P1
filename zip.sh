1a2963e4d77f7de1d731647dacbd2cd6c23b17ae
ldcas-uc3m <100429021@alumnos.uc3m.es>
2024-03-09 18:57:28 +0100

    update zip.sh

--- zip.sh
+++ zip.sh
@@ -2,21 +2,30 @@
 # Bash script to zip the whole project in order to make it deriverable
 # please make sure zip, pv and texlive are installed
 
-OUTFILE="../EPF-1-Arnaiz-Casais.zip"
+OUTFILE="../MT01_3_Arnaiz_Casais.zip"
+
+
+# generate data
+echo "Building turing-machine-simulator/..."
+cd turing-machine-simulator
+make --silent
+cd ..
+
+echo "Performing tests..."
+python3 src/test.py
+
 
 
 # compile the report (and save it to root folder)
 echo "Compiling the report..."
-
 latexmk -cd -shell-escape -pdf report/report.tex 
-
-cp report/report.pdf .
+cp report/report.pdf Memoria_MT_3_Arnaiz_Casais.pdf
 
 
 # zip it (excluding useless stuff)
 echo "Zipping..."
-zip -r "$OUTFILE" . -x zip.sh report/\* \*.git\* img/\* venv/\* *.xml *.jar build/\* .vscode/\* LICENSE
+zip -r "$OUTFILE" . -x zip.sh report/\* \*.git\* img/\* *__pycache__/\* turing-machine-simulator/\* .venv/\* *.xml *.tm *.jar build/\* .vscode/\* LICENSE
 
 # cleanup
 echo "Cleaning up..."
-rm report.pdf
+rm Memoria_MT_3_Arnaiz_Casais.pdf
